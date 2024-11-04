f6a79b7c6ef39b403a4431a4f5d30e0a87294070
Elraffiee Hazem (CC-DA/EET2) <Hazem.Elraffiee@de.bosch.com>
2024-11-04 15:09:44 +0100

    Update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 export default defineConfig({
   base: '/infection-game/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    sourcemap: false,
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
